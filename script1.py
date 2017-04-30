a63daceca745e185d5c4ae24f0f091207a303734
hanafivan <hanafivan@gmail.com>
2017-05-01 00:13:02 +0700

    master

--- script1.py
+++ script1.py
@@ -18,8 +18,18 @@ def both_end(s):
 both_end('spring')
 both_end('ak')
 
+#C
+
 def fix_start(s):
     print(s.replace(s[1:len(s)] , '*'*len(s)))
 
 fix_start('babel')
 fix_start('testing')
+
+#D mix up
+
+def mix_up(a,b):
+    print (a.replace(a[1:2],b[1:2])+','+b.replace(b[1:2],a[1:2]))
+
+mix_up('dog','dinner')
+
